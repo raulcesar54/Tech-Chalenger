86377b087b5345b8eb7a6a1e26b563c539537bd4
raul.souza <raul.souza@terceiro-sky.com.br>
2024-08-03 23:27:32 -0400

    bugfix: ajustando linhas tracada

--- src/components/molecules/stepsContainer/index.tsx
+++ src/components/molecules/stepsContainer/index.tsx
@@ -4,8 +4,8 @@ import { StepItem } from "../../atom";
 export const StepsContainer = () => {
   const selector = useAppSelector((selector) => selector.steps);
   return (
-    <section className="p-4 py-6 bg-white min-w-fit flex flex-row justify-start items-start rounded-[20px] col-span-12 ">
-      <div className="flex flex-row gap-12 relative items-start w-full">
+    <section className="p-4 py-6 bg-white min-w-fit flex flex-row justify-start items-start rounded-[20px] col-span-12 relative">
+      <div className="flex flex-row gap-12 relative items-start ">
         {[...Array(9).keys()].map((_, index) => (
           <StepItem
             label={`Item ${index + 1}`}
